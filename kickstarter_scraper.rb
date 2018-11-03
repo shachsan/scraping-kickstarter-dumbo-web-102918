e53a60f6e2f24415c868698ab5d087e6753ebeb7
SANJAY K SAI <shachsan@hotmail.com>
2018-11-03 23:15:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,7 @@ def create_project_hash
   #iterate over projects
   #retrieve title for each project
   #collect imagelink, description, funded and location from each project
-  #create a project_detail_hash with above attribute as key
+  #create a project_info with above attribute as key
   #create a new hash called project hash  with project title as key and project_detail_hash as value
   projects.each do |project|
     project_title = project.css("h2").text
