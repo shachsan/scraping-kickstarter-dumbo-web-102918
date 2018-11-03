1ea0d3d6cc8b524e310d84acb660506b87bacbd7
SANJAY K SAI <shachsan@hotmail.com>
2018-11-03 23:04:57 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
   projects = page.css(".project")
 
   #iterate over projects
-  
+  #retrie
   #collect imagelink, description, funded and location from each project
   #create a hash with above attribute as key
   #
